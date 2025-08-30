78800a62f99cbd37c44a1d68d54e37b2ed3709bb
mickaelyoshua <mickaelyoshua@gmail.com>
2025-08-30 16:41:59 -0300

    unsafe head tail linked list

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,7 +1,10 @@
+use std::ptr;
+
 type Link<T> = Option<Box<Node<T>>>;
 
 pub struct LinkedList<T> {
     head: Link<T>,
+    tail: *mut Node<T>,
 }
 
 struct Node<T> {
@@ -17,13 +20,63 @@ pub enum SearchType {
 // Standard, conventional way to provide a default value
 impl<T> Default for LinkedList<T> {
     fn default() -> Self {
-        LinkedList { head: None }
+        LinkedList {
+            head: None,
+            tail: ptr::null_mut(),
+            // null mutable pointer. Ignoring Rust safety guarantees
+        }
+    }
+}
+
+impl<T> Drop for LinkedList<T> {
+    fn drop(&mut self) {
+        // take ownership of head with 'take()'
+        let mut current_link = self.head.take();
+
+        // walk the list dropping each node
+        while let Some(mut boxed_node) = current_link {
+            current_link = boxed_node.next.take();
+        }
     }
 }
 
 impl<T> LinkedList<T> {
     pub fn new() -> Self {
         Self::default()
+    }
+
+    // behave like a stack, always push to head
+    pub fn push_left(&mut self, val: T) {
+        let mut new_head = Box::new(Node {
+            val,
+            next: self.head.take(), // from the head take the value and leave a None
+                                    // making the old head empty and this one the head
+        });
+        
+        if self.tail.is_null() {
+            self.tail = &mut *new_head;
+        }
+
+        self.head =  Some(new_head)
+    }
+
+    // append to the end - O(n)
+    pub fn push_right(&mut self, val: T) {
+        let mut new_tail = Box::new(Node { val, next: None });
+        let new_tail_ptr: *mut _ = &mut *new_tail;
+
+        if !self.tail.is_null() {
+            unsafe {
+                // unsafe dereference of raw pointer
+                (*self.tail).next = Some(new_tail);
+            }
+        } else {
+            // if list is empty
+            self.head = Some(new_tail);
+        }
+        
+        // update the tail pointer to new node
+        self.tail = new_tail_ptr;
     }
 }
 
