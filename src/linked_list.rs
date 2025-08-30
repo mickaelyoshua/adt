ab30b508fb36895e2e2eff90f9344f9aeff7c503
mickaelyoshua <mickaelyoshua@gmail.com>
2025-08-30 13:27:52 -0300

    some changes. Find to get reference to value and contains to boolean return

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -29,35 +29,47 @@ impl<T> LinkedList<T> {
 
 impl<T: PartialEq> LinkedList<T> {
     // SEARCH
-    pub fn search(&self, val: &T, search_type: SearchType) -> bool {
+    pub fn find(&self, val: &T) -> Option<&T> {
+        self.find_loop(val)
+    }
+
+    pub fn find_with(&self, val: &T, search_type: SearchType) -> Option<&T> {
         match search_type {
-            SearchType::Loop => self.search_loop(val),
-            SearchType::Recursive => Self::search_recursive(&self.head, val),
+            SearchType::Loop => self.find_loop(val),
+            SearchType::Recursive => Self::find_recursive(&self.head, val)
         }
     }
 
-    fn search_loop(&self, val: &T) -> bool {
+    fn find_loop(&self, val: &T) -> Option<&T> {
         let mut current = &self.head;
 
         while let Some(node) = current {
             if node.val == *val {
-                return true;
+                return Some(&node.val);
             }
             current = &node.next;
         }
-        false
+        None
     }
 
-    fn search_recursive(link: &Link<T>, val: &T) -> bool {
+    fn find_recursive<'a>(link: &'a Link<T>, val: &T) -> Option<&'a T> {
         match link {
-            None => false,
+            None => None,
             Some(node) => {
                 if node.val == *val {
-                    true
+                    Some(&node.val)
                 } else {
-                    Self::search_recursive(&node.next, val)
+                    Self::find_recursive(&node.next, val)
                 }
             }
         }
+    }
+
+    fn contains(&self, val: &T) -> bool {
+        self.find(val).is_some()
+    }
+
+    fn contains_with(&self, val: &T, search_type: SearchType) -> bool {
+        self.find_with(val, search_type).is_some()
     }
 }
