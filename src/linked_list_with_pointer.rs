f34834ae84d61c8b906f91a2cfb52ab814136ae0
mickaelyoshua <mickaelyoshua@gmail.com>
2025-09-03 09:49:59 -0300

    start implementation of linked list with raw pointers

--- src/linked_list_with_pointer.rs
+++ src/linked_list_with_pointer.rs
@@ -1,3 +1,40 @@
-pub struct LinkedList {
-	
+use std::alloc::{dealloc, Layout};
+use std::ptr;
+
+struct Node<T> {
+    val: T,
+    next: *mut Node<T>,
+}
+
+pub struct LinkedList<T> {
+	head: *mut Node<T>,
+	tail: *mut Node<T>,
+}
+
+impl<T> Drop for LinkedList<T> {
+    fn drop(&mut self) {
+        let mut current = self.head;
+
+        while !current.is_null() {
+            let next_node = unsafe { (*current).next };
+
+            let layout = Layout::new::<Node<T>>();
+
+            unsafe { dealloc(current as *mut u8, layout) };
+
+            current = next_node;
+        }
+    }
+}
+
+impl<T> Default for LinkedList<T> {
+    fn default() -> Self {
+        LinkedList { head: ptr::null_mut(), tail: ptr::null_mut() }
+    }
+}
+
+impl<T> LinkedList<T> {
+    fn new() -> Self {
+        Self::default()
+    }
 }
