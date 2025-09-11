6023da5e0cd9695377064f8446388a216a927577
mickaelyoshua <mickaelyoshua@gmail.com>
2025-09-11 12:19:04 -0300

    start implementation with Vec

--- src/stack_with_vector.rs
+++ src/stack_with_vector.rs
@@ -1,3 +1,17 @@
 pub struct Stack<T> {
     elements: Vec<T>,
 }
+
+impl<T> Default for Stack<T> {
+    fn default() -> Self {
+        Self {
+            elements: Vec::new(),
+        }
+    }
+}
+
+impl<T> Stack<T> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
