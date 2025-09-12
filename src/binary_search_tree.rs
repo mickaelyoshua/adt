2995c6b6856f1b641c7526b8b94f0e92a40fbaa3
mickaelyoshua <mickaelyoshua@gmail.com>
2025-09-12 11:01:54 -0300

    initial structure

--- src/binary_search_tree.rs
+++ src/binary_search_tree.rs
@@ -1,3 +1,14 @@
-pub struct BinarySearchTree {
+type Link<T> = Option<Box<Node<T>>>;
 
+struct Node<T> {
+    val: T,
+    parent: *mut Node<T>,
+    left: Link<T>,
+    right: Link<T>,
+    
+
+}
+
+pub struct BinarySearchTree<T> {
+    root: Link<T>,
 }
