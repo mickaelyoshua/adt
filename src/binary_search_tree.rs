e3373982c516a0a99430973f6e61857738c54e48
mickaelyoshua <mickaelyoshua@gmail.com>
2025-09-14 10:17:36 -0300

    some function signatures

--- src/binary_search_tree.rs
+++ src/binary_search_tree.rs
@@ -5,10 +5,36 @@ struct Node<T> {
     parent: *mut Node<T>,
     left: Link<T>,
     right: Link<T>,
-    
-
 }
 
 pub struct BinarySearchTree<T> {
     root: Link<T>,
+}
+
+impl<T> Default for BinarySearchTree<T> {
+    fn default() -> Self {
+        Self { root: None }
+    }
+}
+
+impl<T> BinarySearchTree<T> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &T> {
+        unimplemented!()
+    }
+
+    pub fn search(&self, val: &T) -> Option<&T> {
+        unimplemented!()
+    }
+
+    pub fn insert(&mut self, val: T) {
+        unimplemented!()
+    }
+
+    pub fn delete(&mut self, val: &T) -> Option<T> {
+        unimplemented!()
+    }
 }
