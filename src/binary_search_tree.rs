19b0de7e2a19705c2ae123cb4394e76b7dbc08d4
mickaelyoshua <mickaelyoshua@gmail.com>
2025-09-15 10:03:12 -0300

    search, min and max functions

--- src/binary_search_tree.rs
+++ src/binary_search_tree.rs
@@ -22,12 +22,40 @@ impl<T> BinarySearchTree<T> {
         Self::default()
     }
 
+    pub fn min(&self) -> Option<&T> {
+        let mut node = self.root.as_ref()?;
+        while let Some(left) = &node.left {
+            node = left;
+        }
+        Some(&node.val)
+    }
+    
+    pub fn max(&self) -> Option<&T> {
+        let mut node = self.root.as_ref()?;
+        while let Some(right) = &node.right {
+            node = right;
+        }
+        Some(&node.val)
+    }
+}
+
+impl<T: PartialEq + PartialOrd> BinarySearchTree<T> {
     pub fn iter(&self) -> impl Iterator<Item = &T> {
         unimplemented!()
     }
 
     pub fn search(&self, val: &T) -> Option<&T> {
-        unimplemented!()
+        let mut current_node = &self.root;
+        while let Some(node) = current_node {
+            if node.val == *val {
+                return Some(&node.val);
+            } else if node.val < *val {
+                current_node = &node.right;
+            } else {
+                current_node = &node.left;
+            }
+        }
+        None
     }
 
     pub fn insert(&mut self, val: T) {
