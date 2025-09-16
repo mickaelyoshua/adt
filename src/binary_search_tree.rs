718756ac9aca17612d9640741ddff0a085382cf7
mickaelyoshua <mickaelyoshua@gmail.com>
2025-09-16 07:52:46 -0300

    traverse (iterating) logic for the BST

--- src/binary_search_tree.rs
+++ src/binary_search_tree.rs
@@ -17,9 +17,45 @@ impl<T> Default for BinarySearchTree<T> {
     }
 }
 
+// struct to hold the value for the iteration
+pub struct Iter<'a,T> {
+    stack: Vec<&'a Node<T>>,
+}
+
+// implement the Iterator trait and the next method for the struct
+impl<'a,T> Iterator for Iter<'a,T> {
+    type Item = &'a T;
+
+    // Get the value from the stack.
+    // Go to the right branck from the poped node since all the left side was stacked on the iter
+    // function.
+    // Retrieve the left side of that right node.
+    fn next(&mut self) -> Option<Self::Item> {
+        let node = self.stack.pop()?;
+        let mut current_node = node.right.as_ref();
+        while let Some(n) = current_node {
+            self.stack.push(n);
+            current_node = n.left.as_ref();
+        }
+        Some(&node.val)
+    }
+}
+
 impl<T> BinarySearchTree<T> {
     pub fn new() -> Self {
         Self::default()
+    }
+
+    // populate the Iter struct so can be called the next function of trait Iterator
+    pub fn iter<'a>(&'a self) -> Iter<'a,T> {
+        let mut iterator = Iter { stack: vec![] };
+        let mut current_node = self.root.as_ref();
+
+        while let Some(node) = current_node {
+            iterator.stack.push(node);
+            current_node = node.left.as_ref();
+        }
+        iterator
     }
 
     pub fn min(&self) -> Option<&T> {
@@ -40,10 +76,6 @@ impl<T> BinarySearchTree<T> {
 }
 
 impl<T: PartialEq + PartialOrd> BinarySearchTree<T> {
-    pub fn iter(&self) -> impl Iterator<Item = &T> {
-        unimplemented!()
-    }
-
     pub fn search(&self, val: &T) -> Option<&T> {
         let mut current_node = &self.root;
         while let Some(node) = current_node {
